927fe8434d7933d1bf6dcf52c45b05aaeeaee980
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-31 16:21:39 +0200

    feat: name and desctiption added via class

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 require_once 'models/movie.php';
 require_once 'models/genre.php';
 
-// dichiarazione istanza
-$movie = new Movie();
+// dichiarazione istanza 
+// $movie = new Movie();
 // valorizzazione attributi istanza public
-$movie -> name = "Lord Of The rings";
-$movie -> description = "Best film ever made in the history of the entire human kind";
-$movie -> director = "Peter Jackson";
-$movie -> year = 2001;
+// $movie -> name = "Lord Of The rings";
+// $movie -> description = "Best film ever made in the history of the entire human kind";
+// $movie -> director = "Peter Jackson";
+// $movie -> year = 2001;
 
-var_dump($movie);
+// var_dump($movie);
+
+$movie = new Movie("Peter Jackson", 2001);
+
+$movie->setMovieName("Lord Of The rings");
+$movie->setMovieDesc("Best film ever made in the history of the entire human kind");
+echo $movie->getMovieName() . '<br>' . $movie->getMovieDesc() ;
--- models/movie.php
+++ models/movie.php
@@ -1,7 +1,39 @@
 <?php
+// creazione class Movie public
+// class Movie {
+//     public string $name;
+//     public string $description;
+//     public string $director;
+//     public int $year;
+// }
+
+// creazione class Movie private
 class Movie {
-    public string $name;
-    public string $description;
-    public string $director;
-    public int $year;
+    private string $name;
+    private string $description;
+    private string $director;
+    private int $year;
+    
+    
+    public function setMovieName(string $index_name) {
+        if (strlen($index_name) > 0) {
+            $this->name = $index_name;
+        } else {
+            $this->name = "Nessun titolo presente";
+        }
+    }
+
+    public function getMovieName() {
+        return $this->name;
+    }
+
+    public function setMovieDesc(string $index_desc) {
+        if (strlen($index_desc) > 0) {
+            $this->description = $index_desc;
+        }
+    }
+
+    public function getMovieDesc() {
+        return $this->description;
+    }
 }
